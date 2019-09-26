f9e7e148fd6856e9bd5a4db7070ce114836fa2b3
Mike McTighe <m.mctighe10@gmail.com>
2019-09-26 15:28:02 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ end
     Appointment.all.select {|app| app.doctor == self}
   end
 
-  def new_appointment(date, )
+  def new_appointment(date, pat)
     Appointment.new(date, patient, self )
   end
 
