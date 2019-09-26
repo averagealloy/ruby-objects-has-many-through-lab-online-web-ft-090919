a154a07470d77a39e82b79a98b8358fec3bdf542
Mike McTighe <m.mctighe10@gmail.com>
2019-09-26 14:33:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+ class Song
+
+attr_accessor :name, :artist
+
+@@all = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@all << self
+end
+
+
+def songs
+Song.all.select {|song| song.artist == self}
+end
+
+def genres
+  songs.map {|song| song.genre}
+end
+
+def self.all
+  @@all
+end
+
+
+def new_song(name, genre)
+  Song.new(name, self, genre )
+end 
+
+end
+
+end
