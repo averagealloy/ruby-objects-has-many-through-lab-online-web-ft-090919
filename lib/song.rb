d5950eb5eb2336e05fe5508713c4a92ccb26d43c
Mike McTighe <m.mctighe10@gmail.com>
2019-09-26 14:34:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,14 +15,14 @@ end
 def songs
 Song.all.select {|song| song.artist == self}
 end
+def self.all
+  @@all
+end
 
 def genres
   songs.map {|song| song.genre}
 end
 
-def self.all
-  @@all
-end
 
 
 def new_song(name, genre)
